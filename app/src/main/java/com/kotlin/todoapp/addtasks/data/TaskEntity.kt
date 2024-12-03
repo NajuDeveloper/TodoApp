d2bfba62c229e0828bc150a4fdb8e808c980a2e4
juand-igoox <juand@igoox.com>
2024-12-03 11:03:39 +0100

    configuración de Room

---
+++ app/src/main/java/com/kotlin/todoapp/addtasks/data/TaskEntity.kt
@@ -0,0 +1,12 @@
+package com.kotlin.todoapp.addtasks.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "TaskEntity")
+data class TaskEntity (
+    @PrimaryKey
+    val id: Int,
+    var task: String,
+    var selected: Boolean = false
+)
